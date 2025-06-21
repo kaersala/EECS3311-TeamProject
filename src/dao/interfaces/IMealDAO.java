deb657d5b6bfced5422976a539e6b3f74903e227
Hang <115325771+kaersala@users.noreply.github.com>
2025-06-21 17:51:42 -0400

    Update IMealDAO.java

--- src/dao/interfaces/IMealDAO.java
+++ src/dao/interfaces/IMealDAO.java
@@ -1,4 +1,10 @@
 package dao.interfaces;
 
+import model.meal.Meal;
+import java.util.List;
+
 public interface IMealDAO {
+    List<Meal> getMealsByUserId(int userId);
+    Meal getMealById(int mealId);
+    void saveMeal(Meal meal);
 }
