5bb98ec77022731ee7e342bb5bc32eb8f0ff0783
Hang <115325771+kaersala@users.noreply.github.com>
2025-06-21 18:01:28 -0400

    Update INutirentDAO.java

--- src/dao/interfaces/INutirentDAO.java
+++ src/dao/interfaces/INutirentDAO.java
@@ -1,4 +1,9 @@
 package dao.interfaces;
 
-public interface INutirentDAO {
+import model.user.Nutrient;
+import java.util.List;
+
+public interface INutrientDAO {
+    List<Nutrient> loadAllNutrients();
+    Nutrient getNutrientById(int nutrientId);
 }
