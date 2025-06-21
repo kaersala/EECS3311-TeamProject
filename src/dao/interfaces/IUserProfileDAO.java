fdf72ef377ccae9b896fae20e47b2967f84fe65a
Hang <115325771+kaersala@users.noreply.github.com>
2025-06-21 18:24:39 -0400

    Update IUserProfileDAO.java

--- src/dao/interfaces/IUserProfileDAO.java
+++ src/dao/interfaces/IUserProfileDAO.java
@@ -1,4 +1,12 @@
 package dao.interfaces;
 
+import model.user.UserProfile;
+import java.util.List;
+
 public interface IUserProfileDAO {
+    UserProfile getUserProfile(int userId);
+    List<UserProfile> getAllUserProfiles();
+    void saveUserProfile(UserProfile profile);
+    void updateUserProfile(UserProfile profile);
+    void deleteUserProfile(int userId);
 }
