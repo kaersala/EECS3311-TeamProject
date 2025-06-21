054abf759a80aae947b17a3ed00a159a6e364fe2
Hang <115325771+kaersala@users.noreply.github.com>
2025-06-21 16:39:44 -0400

    Update IFoodDAO.java

--- src/dao/interfaces/IFoodDAO.java
+++ src/dao/interfaces/IFoodDAO.java
@@ -1,4 +1,9 @@
 package dao.interfaces;
 
+import model.user.FoodItem;
+import java.util.List;
+
 public interface IFoodDAO {
+    List<FoodItem> loadFoods();
+    FoodItem getFoodById(int foodId);
 }
